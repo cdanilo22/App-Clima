4329e5d4a1da79dc46a4357e800a78f09d5fe774
CesarDan <cdeleonc4@miumg.edu.gt>
2023-12-27 15:21:55 -0600

    Se agregó la parte de mostrar el clima

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
  import Formulario from './components/Formulario.vue';
  import useClima from './composables/useClima'
+ import Clima from './components/Clima.vue'
 
- const {obtenerClima} = useClima()
-
+ const {obtenerClima, clima, mostrarClima} = useClima()
 
 </script>
 
@@ -12,6 +12,11 @@
   <div class="contenedor buscador-clima"> 
     <Formulario
       @obtener-clima = 'obtenerClima'
+    />
+
+    <Clima
+      v-if="mostrarClima"
+      :clima="clima"
     />
   </div>
 
--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,10 +1,40 @@
-export default function useClima() {
-    
-    const obtenerClima = ({ciudad, pais}) => {
+import {ref, computed} from 'vue'
+import axios from 'axios'
 
+export default function useClima() {
+
+    
+const clima = ref({})
+
+    const obtenerClima = async ({ciudad, pais}) => {
+         //Importar el Api key
+
+        const key = import.meta.env.VITE_API_KEY
+
+         //Obtener la lat, longitud
+
+         try{
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+            const {data} = await axios(url)
+            const {lat, lon} = data[0]
+
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+            const {data: resultado} = await axios(urlClima) //Renombrar la variable data como resultado para que no de conflicto con el anterior destructuring
+            
+            clima.value = resultado;
+
+         }catch(error) {
+            console.log(error); 
+         }
     }
     
+    const mostrarClima = computed(() => {
+        return Object.values(clima.value).length > 0
+    })
+
     return {
-        obtenerClima
+        obtenerClima,
+        clima, 
+        mostrarClima
     }
 }
