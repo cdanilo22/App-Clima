33404b2bd509c60bff61bc684597dbe1bbb9dac9
CesarDan <cdeleonc4@miumg.edu.gt>
2023-12-27 15:23:06 -0600

    Se agrego el componente de Clima

---
+++ src/components/Clima.vue
@@ -0,0 +1,20 @@
+<script setup>
+    defineProps({
+
+        clima: {
+            type: Object,
+            required: true
+        }
+    })
+    </script>
+<template>
+    <div class="resultado">
+        <h2>Clima de: {{clima.name}}</h2>
+        <p class="actual"></p>
+        <div class="temperaturas">
+            <p>Min: <span>&deg;C</span></p>
+            <p>Max: <span>&deg;C</span></p>
+        </div>
+    </div>
+</template> 
+
